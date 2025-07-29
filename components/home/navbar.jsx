b6f26d4b09c57216afcb1ee5037f3b284c3897fa
Fazle Rabbi <mmrabbi625442@gmail.com>
2025-07-29 20:47:56 +0600

    updated navbar

--- components/home/navbar.jsx
+++ components/home/navbar.jsx
@@ -4,14 +4,12 @@ import Image from "next/image";
 import { inter } from "@/app/ui/fonts";
 
 import { LoginButton, MobileNavbarLinks } from "./mobile-navbar-links";
-
-//   src: "../tt_trailer.ttf",
-// });
+import { ScrollNavbar } from "./scroll-navbar";
 
 export function Navbar() {
   return (
-    <nav
-      className={`${inter.className} font-normal text-md bg-[#ffffff] dark:bg-[#071a26] navbar morphic-effect sticky top-0 w-full z-50 shadow-md`}
+    <ScrollNavbar
+      className={`${inter.className} font-normal text-md navbar  sticky top-0 w-full z-50`}
     >
       <div className="w-full mx-auto px-3 lg:px-10 flex items-center justify-between h-[70px]">
         {/* <!-- Logo  --> */}
@@ -24,16 +22,12 @@ export function Navbar() {
               height={80}
               width={80}
             />
-            {/* <i className="fas fa-cube text-gray-800 text-2xl"></i> */}
-            {/* <span className={`${myFont.className} ml-2 text-3xl text-gray-800`}>
-              CSE Archive
-            </span> */}
           </Link>
         </div>
         <DesktopNavbarLinks />
         <MobileNavbarLinks />
       </div>
-    </nav>
+    </ScrollNavbar>
   );
 }
 
@@ -59,55 +53,6 @@ function DesktopNavbarLinks() {
           >
             Book Shelf
           </Link>
-          {/*           <button className="text-gray-700 dark:text-gray-200 hover:text-indigo-600 hover:bg-gray-200 dark:hover:bg-gray-800 px-3.5 py-2.5 rounded-md text-sm font-medium flex items-center transition-all duration-500">
-            Book shelf
-            <i className="fas fa-chevron-down ml-1 text-xs"></i>
-          </button> */}
-
-          {/*           <div className="dropdown morphic-effect absolute left-0 mt-3 w-48 rounded-md shadow-lg bg-white dark:bg-[#071a26] z-50">
-            <Link
-              href="#"
-              className="block w-full px-4 py-2 rounded text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-200 dark:hover:bg-gray-800 hover:text-indigo-600"
-            >
-              Physics
-            </Link>
-            <Link
-              href="#"
-              className="block px-4 py-2 rounded text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-200 dark:hover:bg-gray-800 hover:text-indigo-600"
-            >
-              Chemistry
-            </Link>
-            <Link
-              href="#"
-              className="block px-4 py-2 rounded text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-200 dark:hover:bg-gray-800 hover:text-indigo-600"
-            >
-              Maths
-            </Link>
-            <Link
-              href="#"
-              className="block px-4 py-2 rounded text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-200 dark:hover:bg-gray-800 hover:text-indigo-600"
-            >
-              CSE
-            </Link>
-            <Link
-              href="#"
-              className="block px-4 py-2 rounded text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-200 dark:hover:bg-gray-800 hover:text-indigo-600"
-            >
-              Hardware
-            </Link>
-            <Link
-              href="#"
-              className="block px-4 py-2 rounded text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-200 dark:hover:bg-gray-800 hover:text-indigo-600"
-            >
-              Productivity
-            </Link>
-            <Link
-              href="#"
-              className="block px-4 py-2 rounded text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-200 dark:hover:bg-gray-800 hover:text-indigo-600"
-            >
-              English
-            </Link>
-          </div> */}
         </div>
 
         {/* <!-- Nav Item 3 --> */}
@@ -167,12 +112,6 @@ function DesktopNavbarLinks() {
             <i className="fas fa-chevron-down ml-1 text-xs"></i>
           </button>
           <div className="dropdown morphic-effect absolute left-0 mt-3 w-44 rounded-md shadow-lg bg-white dark:bg-[#071a26] z-50">
-            {/*             <Link
-              href="#"
-              className="block px-4 py-2 rounded text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-200 dark:hover:bg-gray-800 hover:text-indigo-600"
-            >
-              Email
-            </Link> */}
             <Link
               href="#"
               target="_blank"
@@ -180,25 +119,6 @@ function DesktopNavbarLinks() {
             >
               Coming Soon
             </Link>
-            {/*             <Link
-              href="#"
-              className="block px-4 py-2 rounded text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-200 dark:hover:bg-gray-800 hover:text-indigo-600"
-            >
-              Facebook Page
-            </Link>
-
-            <Link
-              href="/contact/developers"
-              className="block px-4 py-2 rounded text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-200 dark:hover:bg-gray-800 hover:text-indigo-600"
-            >
-              Contributors
-            </Link> */}
-            {/*             <Link
-              href="#"
-              className="block px-4 py-2 rounded text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-200 dark:hover:bg-gray-800 hover:text-indigo-600"
-            >
-              Why this?
-            </Link> */}
           </div>
         </div>
 
